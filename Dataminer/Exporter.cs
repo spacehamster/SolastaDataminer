fe92d12b295f99481bd1336f061491e218ca115c
spacehamster <mail.spacehamster@gmail.com>
2020-11-10 16:00:57 +1100

    Add project

---
+++ Dataminer/Exporter.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Dataminer
+{
+    internal class Exporter
+    {
+        internal static void Export()
+        {
+            Main.Log("Dumping database");
+            if(!Directory.Exists("Dump"))
+            {
+                Directory.CreateDirectory("Dump");
+            }
+            var databases = (Dictionary<Type, object>)AccessTools.Field(typeof(DatabaseRepository), "databases")
+                .GetValue(null);
+            using (var sw = new StreamWriter("Dump/Types.txt"))
+            {
+                foreach(var type in databases.Keys)
+                {
+                    sw.WriteLine($"{type.FullName}");
+                }
+            }
+            using (var sw = new StreamWriter("Dump/Assets.txt"))
+            {
+                sw.WriteLine("{0}\t{1}\t{2}",
+                    "Name", "Type", "GUID");
+                foreach (object db in databases.Values)
+                {
+                    var elements = (IList)AccessTools.Method(db.GetType(), "GetAllElements")
+                        .Invoke(db, new object[] { });
+                    foreach(BaseDefinition value in elements)
+                    {
+                        sw.WriteLine("{0}\t{1}\t{2}",
+                            value.Name, value.GetType().FullName, value.GUID);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Dataminer/Main.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityModManagerNet;
+
+namespace Dataminer
+{
+#if DEBUG
+    [EnableReloading]
+#endif
+    static class Main
+    {
+        static UnityModManager.ModEntry ModEntry;
+        public static void Log(string msg)
+        {
+            ModEntry?.Logger.Log(msg);
+        }
+        static void Load(UnityModManager.ModEntry modEntry)
+        {
+            ModEntry = modEntry;
+            modEntry.OnGUI = OnGUI;
+#if DEBUG
+            modEntry.OnUnload = Unload;
+#endif
+        }
+#if DEBUG
+        static bool Unload(UnityModManager.ModEntry modEntry)
+        {
+            return true;
+        }
+#endif
+        static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            if (GUILayout.Button("Export Database"))
+            {
+                Exporter.Export();
+            }
+        }
+    }
+}
